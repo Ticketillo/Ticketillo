adcd0d3e907325eaa08450483bae925f46eca2e8
Longbo <longbo.qiu@waapiti.eu>
2023-05-13 17:08:51 +0200

    Add `profile-page`

--- packages/frontend/src/modules/profile/pages/ProfilePage.tsx
+++ packages/frontend/src/modules/profile/pages/ProfilePage.tsx
@@ -1,27 +1,53 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "components/card";
+import { useForm } from "react-hook-form";
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "components/card";
 import { Button } from "components/button";
 import { Label } from "components/label";
 import { Input } from "components/input";
+import { Textarea } from "components/textarea";
+import { Avatar, AvatarFallback, AvatarImage } from "components/avatar";
 
 export default function ProfilePage() {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data: any) => {
+        // edit user information
+        console.log(data);
+    };
+
     return (
         <div className="w-full md:w-1/2">
             <Card>
                 <CardHeader>
-                    <CardTitle>Account</CardTitle>
+                    <CardTitle className="flex gap-2 items-center">
+                        <Avatar className="w-8 h-8">
+                            <AvatarImage src={`https://avatar.vercel.sh/12312.png`} alt="user-avatar" />
+                            <AvatarFallback>SC</AvatarFallback>
+                        </Avatar>
+                        John Doe
+                    </CardTitle>
                 </CardHeader>
-                <CardContent className="grid gap-4">
-                    <div className="grid gap-2">
-                        <Label htmlFor="email">Name</Label>
-                        <Input id="email" type="email" placeholder="m@example.com" />
-                    </div>
-                    <div className="grid gap-2">
-                        <Label htmlFor="password">Description</Label>
-                        <Input id="password" type="password" />
-                    </div>
-                </CardContent>
+                <form>
+                    <CardContent className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="name">Name</Label>
+                            <Input id="name" {...register("name")} />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="description">Description</Label>
+                            <Textarea id="description" {...register("description")} />
+                        </div>
+
+                        <div className="grid gap-2">
+                            <Label htmlFor="avatar">Avatar</Label>
+                            <Input id="avatar" {...register("avatar")} type="file" accept="image/*" />
+                        </div>
+                    </CardContent>
+                </form>
                 <CardFooter className="flex justify-end">
-                    <Button className="w-fit">Save</Button>
+                    <Button className="w-fit" onClick={handleSubmit(onSubmit)}>
+                        Save
+                    </Button>
                 </CardFooter>
             </Card>
         </div>
