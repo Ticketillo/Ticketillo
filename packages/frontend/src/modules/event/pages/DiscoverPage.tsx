fa4e06887c059c455dcb586da45e8c6d32e54550
Longbo <longbo.qiu@waapiti.eu>
2023-05-13 16:09:08 +0200

    Add skeleton to events pages

--- packages/frontend/src/modules/event/pages/DiscoverPage.tsx
+++ packages/frontend/src/modules/event/pages/DiscoverPage.tsx
@@ -1,6 +1,7 @@
 import useGetDiscoverEvents from "../query/useGetDiscoverEvents";
 
 import EventCard from "../components/EventCard/EventCard";
+import { Skeleton } from "components/skeleton";
 
 export default function DiscoverPage() {
     const { data: events, isLoading } = useGetDiscoverEvents("user");
@@ -8,7 +9,7 @@ export default function DiscoverPage() {
     return (
         <div className="w-full grid gap-4 grid-cols-1 md:grid-cols-2">
             {isLoading ? (
-                <div>Loading..</div>
+                Array.from({ length: 10 }).map((_, index) => <Skeleton key={index} className="w-full h-64 rounded-lg" />)
             ) : events?.length === 0 ? (
                 <div>You have no events</div>
             ) : (
