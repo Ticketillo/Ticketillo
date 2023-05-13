262ec046d5317fd6c99a73af3cddb751a427cdec
Longbo <longbo.qiu@waapiti.eu>
2023-05-13 11:47:27 +0200

    Add main `layout`

--- packages/frontend/src/modules/home/pages/HomePage.tsx
+++ packages/frontend/src/modules/home/pages/HomePage.tsx
@@ -1,17 +1,13 @@
-import React from "react";
+import Logo from "../components/Logo";
+import UserMenu from "../components/UserMenu";
 
-import { Avatar, AvatarFallback, AvatarImage } from "components/avatar";
-
-const HomePage = () => {
+export default function HomePage() {
     return (
-        <div className="text-red-500 text-2xl font-bold">
-            <p>dalksdahjs</p>
-            <Avatar className="mr-2 h-5 w-5">
-                <AvatarImage src={`https://avatar.vercel.sh/1.png`} alt="dasdas" />
-                <AvatarFallback>SC</AvatarFallback>
-            </Avatar>
-        </div>
+        <main className="sm:mx-auto flex w-full max-w-5xl flex-col items-start p-5">
+            <header className="flex items-center justify-between w-full">
+                <Logo />
+                <UserMenu />
+            </header>
+        </main>
     );
-};
-
-export default HomePage;
+}
