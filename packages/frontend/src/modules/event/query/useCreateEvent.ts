f92019ddacfdcddbcc6dfebdb491e3b92a1a5ce0
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-05-13 18:13:49 +0200

    WIP

--- packages/frontend/src/modules/event/query/useCreateEvent.ts
+++ packages/frontend/src/modules/event/query/useCreateEvent.ts
@@ -1,19 +1,19 @@
 import { useMutation } from "@tanstack/react-query";
-import { EventApi } from "api/service";
+import { useAuthState } from "modules/auth/state";
 import { EventService } from "services/EventService";
 
 interface EventCreateParams {
-    name: string | undefined;
-    description: string | undefined;
-    date: Date | undefined;
-    attendees: number | undefined;
-    image: string;
-    price: number | undefined;
-    location: string | undefined;
+    name: string;
+    description: string;
+    attendees: number;
+    image: File;
+    price: number;
 }
 
 export default function useCreateEvent() {
-    return useMutation(({ name, description, date, attendees, image, price, location }: EventCreateParams) =>
-        EventApi.createEvent(_, name, description, image, _, attendees, price),
+    const { address } = useAuthState();
+
+    return useMutation(({ name, description, attendees, image, price }: EventCreateParams) =>
+        EventService.createEvent(address!, name, description, image, "", attendees, price.toString()),
     );
 }
--- packages/frontend/src/modules/home/components/TabsNavigation.tsx
+++ packages/frontend/src/modules/home/components/TabsNavigation.tsx
@@ -8,28 +8,31 @@ export default function TabsNavigation({ className }: React.HTMLAttributes<HTMLE
     const { isLoggedIn } = useAuthState();
 
     return (
-        <nav className={cn("flex items-center justify-between space-x-4 lg:space-x-6 ", className)}>
-            <div className="flex flex-row gap-6">
-                <Link
-                    to="/discover"
-                    className={cn(
-                        "text-sm font-medium transition-colors hover:text-primary",
-                        location.pathname === "/discover" || location.pathname === "/"
-                            ? "text-primary font-semibold"
-                            : "text-muted-foreground",
-                    )}
-                >
-                    Discover
-                </Link>
-                <Link
-                    to="/your-events"
-                    className={cn(
-                        "text-lg font-medium transition-colors hover:text-primary",
-                        location.pathname === "/your-events" ? "text-primary font-semibold" : "text-muted-foreground",
-                    )}
-                >
-                    Your events
-                </Link>
+        <nav className={cn("flex flex-row w-full items-center justify-between space-x-4 lg:space-x-6 ", className)}>
+            <div className="flex flex-row w-full justify-between items-center">
+                <div className="flex flex-row gap-6">
+                    <Link
+                        to="/discover"
+                        className={cn(
+                            "text-sm font-medium transition-colors hover:text-primary",
+                            location.pathname === "/discover" || location.pathname === "/"
+                                ? "text-primary font-semibold"
+                                : "text-muted-foreground",
+                        )}
+                    >
+                        Discover
+                    </Link>
+                    <Link
+                        to="/your-events"
+                        className={cn(
+                            "text-sm font-medium transition-colors hover:text-primary",
+                            location.pathname === "/your-events" ? "text-primary font-semibold" : "text-muted-foreground",
+                        )}
+                    >
+                        Your events
+                    </Link>
+                </div>
+                <CreateEventModal />
             </div>
         </nav>
     );
