ee4845fbc90c118bb966a01624ef27baaac72eca
Longbo <longbo.qiu@waapiti.eu>
2023-05-13 11:41:05 +0200

    Fix tailwind

--- packages/frontend/src/modules/home/pages/HomePage.tsx
+++ packages/frontend/src/modules/home/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "components/avatar";
 const HomePage = () => {
     return (
         <div className="text-red-500 text-2xl font-bold">
+            <p>dalksdahjs</p>
             <Avatar className="mr-2 h-5 w-5">
                 <AvatarImage src={`https://avatar.vercel.sh/1.png`} alt="dasdas" />
                 <AvatarFallback>SC</AvatarFallback>
