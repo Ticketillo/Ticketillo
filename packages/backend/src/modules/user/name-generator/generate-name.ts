40f191e722fd67db114773d6eedc355d1e5ec757
AdriaCarrera <adria.cm95@gmail.com>
2023-05-13 17:50:01 +0200

    user

--- packages/backend/src/modules/user/name-generator/generate-name.ts
+++ packages/backend/src/modules/user/name-generator/generate-name.ts
@@ -1,7 +1,15 @@
-import { getRandomNumber } from "../../../common/util/random";
 import nouns from "./nouns";
 import adjectives from "./adjectives";
-import capitalize from "../../../common/util/capitalize";
+
+function getRandomNumber(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function capitalize(word: string) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
 
 export default function generateName(): string {
     return capitalize(adjectives[getRandomNumber(0, adjectives.length - 1)]) + capitalize(nouns[getRandomNumber(0, nouns.length - 1)]);
