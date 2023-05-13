6a82da273b113ff458e87a0ba183595bcc861032
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-05-13 10:29:39 +0200

    More setup

--- packages/frontend/src/router/Router.tsx
+++ packages/frontend/src/router/Router.tsx
@@ -1,26 +1,16 @@
-import { ModalProvider } from "@peersyst/react-components";
 import { BrowserRouter, useRoutes } from "react-router-dom";
-import { config } from "common/config";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
-import Header from "../common/components/navigation/Header/Header";
-import Footer from "../common/components/navigation/Footer/Footer";
-import { useCounterRoutes } from "ui/counter/router/CounterRouter";
+import { config } from "config";
 
 const Routes = () => {
-    const dashboardRoutes = useCounterRoutes();
-
-    return useRoutes([...dashboardRoutes]);
+    return useRoutes([]);
 };
 
 const Router = (): JSX.Element => {
     return (
         <BrowserRouter basename={config.publicUrl}>
-            <ModalProvider>
-                <ScrollToTop />
-                <Header />
-                <Routes />
-                <Footer />
-            </ModalProvider>
+            <ScrollToTop />
+            <Routes />
         </BrowserRouter>
     );
 };
