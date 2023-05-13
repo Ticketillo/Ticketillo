d10f92b714d45250baf7fa2034dc11c1bd7d88a7
obnol <longboqiu@hotmail.com>
2023-05-13 12:05:01 +0200

    test

--- packages/frontend/src/modules/home/components/UserMenu.tsx
+++ packages/frontend/src/modules/home/components/UserMenu.tsx
@@ -5,5 +5,5 @@ import { Button } from "components/button";
 export default function UserMenu() {
     const { isLoggedIn } = useAuthState();
 
-    return <div>{isLoggedIn ? <p>dalksdja</p> : <Button>Login</Button>}</div>;
+    return <div>{isLoggedIn ? <p>dalksddasdja</p> : <Button>Login</Button>}</div>;
 }
